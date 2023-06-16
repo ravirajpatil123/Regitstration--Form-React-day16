65c2c8bc3d5a742954a9a3a97dffa88d33e8ba12
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-16 11:35:05 +0530

    multiple Pages App

--- registration-form/src/App.js
+++ registration-form/src/App.js
@@ -1,64 +1,26 @@
-import {useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyTodo from "./pages/MyTodo";
+import MyRegistration from "./pages/MyRegistration";
+import MyNavigationLinks from "./pages/MyNavigationLinks";
+import MyTodoList from "./pages/MyTodoList";
+
 function App() {
   return (
     <>
-      <h1>Login</h1>
-      <MyLogIn />
-    </>
-  );
-}
-
-function MyLogIn() {
-  let [user ,setUser] = useState[{user:"",password:""}];
-
-  let handleUsernameAction = (e) => {
-  let newuser ={...user,username:e.target.value};
-  setUser(newuser);
-  }
-
-  let handlePasswordAction = (e) => {
-    let newuser ={...user,password:e.target.value};
-    setUser(newuser);
-    }
-  let loginAction =async () =>{
-    let url= `http://localhost:4000/adduser?username=${user.username}&password=${user.password}`;
-    await fetch(url);
-
-   let newuser ={
-    username:"",
-    password:""
-   }
-setUser(newuser);
-
-    setUser()
-
- }
-  return (
-    <>
-      <input
-        className="form-control mb-1"
-        style={{ borderBlockColor: "black" }}
-        type="text"
-        placeholder="Enter Username"
-        onChange={handleUsernameAction}
-        value={user.username}
+      <BrowserRouter>
+        <MyNavigationLinks />
         
-      />
-      <input
-        className="form-control mb-1"
-        style={{ borderBlockColor: "black" }}
-        type="password"
-        placeholder="Enter password"
-        onChange={handlePasswordAction}
-        value={user.password}
-      />
 
-      <input
-        className="form-control bg-danger text-light fw-bold "
-        type="button"
-        value="Login"
-       onClick={loginAction}
-      />
+        <Routes>
+          {/** 1 Route means 1 Page */}
+          <Route path="/" element={<h1>Hello</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/todo" element={<MyTodo />} />
+          <Route path="/todo-list" element={<MyTodoList />} />
+          <Route path="/registration" element={<MyRegistration />} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ registration-form/src/pages/MyNavigationLinks.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function MyNavigationLinks() {
+  return (
+    <>
+      <Link to={"/home"}>Home |</Link>
+      <Link to={"/todo"}>Todo |</Link>
+      <Link to={"/todo-list"}>Todo List |</Link>
+      <Link to={"/registration"}>Registration</Link>
+    </>
+  );
+}
+
+export default MyNavigationLinks;
