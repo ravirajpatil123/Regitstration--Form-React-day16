4361b6da339f6dc598424c42d3ceee61e39ccc3c
Raviraj patil <ravirajpatil298@gmail.com>
2023-06-17 10:45:29 +0530

    setvervaidation

--- registration-form/src/pages/MyRegistration.js
+++ registration-form/src/pages/MyRegistration.js
@@ -1,7 +1,10 @@
-import { useRef,useState } from "react";
+import { useRef, useState } from "react";
 
 function MyRegistration() {
-  let formRef= useRef();
+  let formRef = useRef();
+  let [isSuccess, setIsSuccess] = useState(false);
+  let [isError, setIsError] = useState(false);
+
   let [user, setUser] = useState({
     username: "",
     password: "",
@@ -30,69 +33,91 @@ function MyRegistration() {
   };
 
   let registerAction = async () => {
-    // BACKEND
-    formRef.current.classList.add("was-validated");
-    let formStatus=formRef.current.checkValidity();
-    if(!formStatus){
-      return;
+    try {
+      formRef.current.classList.add("was-validated");
+      let formStatus = formRef.current.checkValidity();
+      if (!formStatus) {
+        return;
+      }
+
+      // BACKEND
+      let url = `http://localhost:4000/adduser?username=${user.username}&password=${user.password}&email=${user.email}&mobile=${user.mobile}`;
+
+      let res = await fetch(url);
+
+      if (res.status !== 200) {
+        let serverMsg = await res.text();
+        throw new Error(serverMsg);
+      }
+
+      let newuser = {
+        username: "",
+        password: "",
+        email: "",
+        mobile: "",
+      };
+      setUser(newuser);
+
+      formRef.current.classList.remove("was-validated");
+
+      alert("success");
+      setIsSuccess(true);
+    } catch (err) {
+      alert(err.message);
+      setIsError(true);
+    } finally {
+      setTimeout(() => {
+        setIsSuccess(false);
+        setIsError(false);
+      }, 5000);
     }
-
-    let url = `http://localhost:4000/adduser?username=${user.username}&password=${user.password}&email=${user.email}&mobile=${user.mobile}`;
-    await fetch(url);
-
-    let newuser = {
-      username: "",
-      password: "",
-      email: "",
-      mobile: "",
-    };
-    setUser(newuser);
   };
-  
 
   return (
     <>
-    <form ref={formRef} className="-validation">
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Enter username"
-        value={user.username}
-        onChange={handlerUsernameAction}
-        required
-      />
-      <input
-        type="password"
-        className="form-control"
-        placeholder="Enter password"
-        value={user.password}
-        onChange={handlerPasswordAction}
-        required
-      />
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Enter Email"
-        value={user.email}
-        onChange={handlerEmailAction}
-        required
-      />
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Enter mobile"
-        value={user.mobile}
-        onChange={handlerMobileAction}
-        required
-      />
-      <input
-        type="button"
-        value="Register"
-        className="w-100"
-        onClick={registerAction}
-        required
-      />
+      <form ref={formRef} className="needs-validation">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter username"
+          value={user.username}
+          onChange={handlerUsernameAction}
+          required
+        />
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          value={user.password}
+          onChange={handlerPasswordAction}
+          required
+        />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter Email"
+          value={user.email}
+          onChange={handlerEmailAction}
+          required
+        />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter mobile"
+          value={user.mobile}
+          onChange={handlerMobileAction}
+          required
+        />
+        <input
+          type="button"
+          value="Register"
+          className="w-100"
+          onClick={registerAction}
+        />
       </form>
+
+      {isSuccess && <div className="alert alert-success">Success</div>}
+      {isError && <div className="alert alert-danger">Error</div>}
     </>
   );
 }
